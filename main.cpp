bc08af13d3608ba336a07ff0dc372283488523fe
AG <if23b213@technikum-wien.at>
2025-04-04 10:38:33 +0200

    for Schleife, für die n Ausgaben

--- main.cpp
+++ main.cpp
@@ -3,14 +3,19 @@
 using namespace std;
 
 void fizzBuzz(int n){
-    if(n%3 == 0 && n%5 == 0){
-        cout << "Fizz/Buzz" << endl;
-    }
-    if(n%3 == 0){
-        cout << "Fizz" << endl;
-    }
-    if(n%5 == 0){
-        cout << "Buzz" << endl;
+    for(int i=0; i<n; i++){
+        if(n%3 == 0 && n%5 == 0){
+            cout << "Fizz/Buzz" << endl;
+        }
+        if(n%3 == 0){
+            cout << "Fizz" << endl;
+            break;
+        }
+        if(n%5 == 0){
+            cout << "Buzz" << endl;
+            break;
+        }
+        cout << i << endl;
     }
 }
 
