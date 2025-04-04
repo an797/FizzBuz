34841dcca803a36a9fe22e8d3db1e891f38d664f
AG <if23b213@technikum-wien.at>
2025-04-04 10:46:38 +0200

    Verbesserungen (else if statt nur if)

--- main.cpp
+++ main.cpp
@@ -4,18 +4,20 @@ using namespace std;
 
 void fizzBuzz(int n){
     for(int i=1; i<=n; i++){
-        cout << i << endl;
         if(i%3 == 0 && i%5 == 0){
             cout << "Fizz/Buzz" << endl;
             continue;
         }
-        if(i%3 == 0){
+        else if(i%3 == 0){
             cout << "Fizz" << endl;
             continue;
         }
-        if(i%5 == 0){
+        else if(i%5 == 0){
             cout << "Buzz" << endl;
             continue;
+        }
+        else{
+            cout << i << endl;
         }
     }
 }
